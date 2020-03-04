344c2ba8db29870d4b3c7783f9599dc205bb3d85
JulienMuller63 <julien.muller63@orange.fr>
2020-03-04 12:51:46 +0100

    html en cours
    Co-authored-by: elisamilano@hotmail.fr <elisamilano@hotmail.fr>
    Co-authored-by: Fabienzaragoza <fabien.zaragoza@free.fr>
    Co-authored-by: SergeHuriaux <sergeoclockpromox@gmail.com>

--- app/js/app.js
+++ app/js/app.js
@@ -1,13 +1,10 @@
 var app = {
   init: function() {
     console.log('inittt');
-    var svgContainer = document.getElementById('svgContainer');
-    var animItem = bodymovin.loadAnimation({
-        wrapper: svgContainer,
-        animType: 'svg',
-        loop: true,
-        animationData: JSON.parse(animationData)
-    });
+    $('.flip').hover(function(){
+      $(this).find('.card').toggleClass('flipped');
+
+});
   }
 };
 
