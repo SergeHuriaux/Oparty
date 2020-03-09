e7c2cfa948a2cc6f34f2ac10f3f76755379d106b
JulienMuller63 <julien.muller63@orange.fr>
2020-03-09 10:54:38 +0100

    install WP

---
+++ content/themes/oparty/index.php
@@ -0,0 +1,12 @@
+
+
+
+<section class="banner">
+    <div class="intro">
+    <h1 class="intro__title">Template index.php</h1>
+    <p class="intro__content">Je suis l'index !! Il te manques donc un fichier de template !!</p>
+    </div>
+    <div class="action-bar">
+    <a href="<?= home_url(); ?>" class="action-button">Retourner à l'accueil</a>
+    </div>
+</section>
