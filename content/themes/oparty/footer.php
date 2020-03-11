c6df9dbedf80355866471a543b328af6312ff142
JulienMuller63 <julien.muller63@orange.fr>
2020-03-11 14:58:23 +0100

    Modif newsletter in footer

--- content/themes/oparty/footer.php
+++ content/themes/oparty/footer.php
@@ -8,7 +8,16 @@
         <li><a href="#"><i class="fa fa-gavel"></i></a></li>
       </ul>
     </div> 
-     
+    <form>            
+      <div class="form-row justify-content-center">        
+        <div class="form-group col-md-3">
+          <input type="mail" class="form-control" id="newsletter" placeholder="Incrivez-vous à notre newsletter" required>
+        </div>        
+        <div class="form-group col-md-2">
+          <button type="submit" class="btn btn-primary">Envoyer</button>
+        </div>
+      </div>     
+    </form> 
   </footer> 
   <?php wp_footer(); ?>
 </body>
