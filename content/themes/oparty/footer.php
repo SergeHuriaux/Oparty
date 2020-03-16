26da383829c74e8031e934fa907597d8ca5adc00
JulienMuller63 <julien.muller63@orange.fr>
2020-03-16 11:51:34 +0100

    Modif liens

--- content/themes/oparty/footer.php
+++ content/themes/oparty/footer.php
@@ -5,7 +5,7 @@
         <li><a href="https://www.instagram.com" target="_blank"><i class="fa fa-instagram"></i></a></li>
         <li><a href="https://twitter.com" target="_blank"><i class="fa fa-twitter"></i></a></li>
         <li><a href="https://www.linkedin.com" target="_blank"><i class="fa fa-linkedin"></i></a></li>
-        <li><a href="http://localhost/oparty/mentions-legales/"><i class="fa fa-gavel"></i></a></li>
+        <li><a href="http://54.166.157.119/projet-oparty/mentions-legales/"><i class="fa fa-gavel"></i></a></li>
       </ul>
     </div> 
     <?php get_template_part('template-parts/newsletter/newsletter'); ?>
